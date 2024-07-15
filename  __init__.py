f8b3617577802408562c808e0449af9565d6f173
Nikolay Stepankov <gmpcrm@gmail.com>
2024-07-15 13:25:06 +0200

    update: create dataset

---  __init__.py
+++  __init__.py
@@ -3,3 +3,4 @@ from .remove_duplicates import run, run_config, remove
 from .detect_florence import run, run_config
 from .create_dataframe import run, run_config
 from .filter_labels import run, run_config
+from .create_dataset import run, run_config
--- create_dataset.py
+++ create_dataset.py
@@ -4,15 +4,23 @@ import os
 from pathlib import Path
 import re
 from PIL import Image
+from tqdm import tqdm
 
 
 class Config:
-    def __init__(self):
-        self.source_folder = "c:\\proplex\\florence"
-        self.data_folder = "c:\\proplex\\florence.good"
-        self.dataset_folder = "c:\\proplex\\dataset"
-        self.valid_percent = 20  # Default validation percentage
-        self.class_id = 0  # Default class ID for YOLO
+    def __init__(
+        self,
+        source_folder="~/data/florence",
+        data_folder="~/data/florence.new",
+        dataset_folder="~/data/dataset",
+        valid=20,  # Default validation percentage
+        class_id=0,  # Default class ID for YOLO
+    ):
+        self.source_folder = source_folder
+        self.data_folder = data_folder
+        self.dataset_folder = dataset_folder
+        self.valid = valid
+        self.class_id = class_id
 
 
 class YOLODatasetCreator:
@@ -33,7 +41,7 @@ class YOLODatasetCreator:
         return (x_center, y_center, width, height)
 
     def create_dataset(self):
-        dataset_folder = Path(self.config.dataset_folder)
+        dataset_folder = Path(self.config.dataset_folder).expanduser()
         train_images_folder = dataset_folder / "train" / "images"
         train_labels_folder = dataset_folder / "train" / "labels"
         valid_images_folder = dataset_folder / "valid" / "images"
@@ -46,61 +54,73 @@ class YOLODatasetCreator:
 
         file_pattern = re.compile(r"^(.*)\.florence\.(\d+)\.png$")
         counter = 0
-        valid_interval = 100 // self.config.valid_percent
+        valid_interval = 100 // self.config.valid
 
-        for file_name in os.listdir(self.config.data_folder):
-            if file_name.endswith(".png"):
-                match = file_pattern.match(file_name)
-                if not match:
-                    continue
+        files = [
+            f
+            for f in os.listdir(Path(self.config.data_folder).expanduser())
+            if f.endswith(".png")
+        ]
 
-                original_name = match.group(1)
-                index_str = match.group(2)
-                index = int(index_str)
+        for file_name in tqdm(files, desc="Processing files"):
+            match = file_pattern.match(file_name)
+            if not match:
+                continue
 
-                json_file_name = f"{original_name}.json"
-                json_file_path = Path(self.config.source_folder) / json_file_name
-                original_img_path = (
-                    Path(self.config.source_folder) / f"{original_name}.png"
-                )
+            original_name = match.group(1)
+            index_str = match.group(2)
+            index = int(index_str)
 
-                if json_file_path.exists() and original_img_path.exists():
-                    with open(json_file_path, "r", encoding="utf-8") as json_file:
-                        data = json.load(json_file)
+            json_file_name = f"{original_name}.json"
+            json_file_path = (
+                Path(self.config.source_folder).expanduser() / json_file_name
+            )
+            original_img_path = (
+                Path(self.config.source_folder).expanduser() / f"{original_name}.png"
+            )
 
-                    img = Image.open(original_img_path)
-                    output_img_path = train_images_folder / f"{original_name}.jpg"
-                    label_file_path = train_labels_folder / f"{original_name}.txt"
+            if json_file_path.exists() and original_img_path.exists():
+                with open(json_file_path, "r", encoding="utf-8") as json_file:
+                    data = json.load(json_file)
 
-                    if counter % valid_interval == 0:
-                        output_img_path = valid_images_folder / f"{original_name}.jpg"
-                        label_file_path = valid_labels_folder / f"{original_name}.txt"
+                img = Image.open(original_img_path)
+                output_img_path = train_images_folder / f"{original_name}.jpg"
+                label_file_path = train_labels_folder / f"{original_name}.txt"
 
-                    img.save(output_img_path, "JPEG")
+                if counter % valid_interval == 0:
+                    output_img_path = valid_images_folder / f"{original_name}.jpg"
+                    label_file_path = valid_labels_folder / f"{original_name}.txt"
 
-                    florence_results = next(
-                        iter(data["florence_results"].values()), None
-                    )
-                    if florence_results:
-                        bboxes = florence_results["bboxes"]
-                        if index < len(bboxes):
-                            bbox = bboxes[index]
-                            yolo_bbox = self.convert_bbox_to_yolo(
-                                (data["width"], data["height"]), bbox
+                img.save(output_img_path, "JPEG")
+
+                florence_results = next(iter(data["florence_results"].values()), None)
+                if florence_results:
+                    bboxes = florence_results["bboxes"]
+                    if index < len(bboxes):
+                        bbox = bboxes[index]
+                        yolo_bbox = self.convert_bbox_to_yolo(
+                            (data["width"], data["height"]), bbox
+                        )
+
+                        with open(label_file_path, "a", encoding="utf-8") as label_file:
+                            label_file.write(
+                                f"{self.config.class_id} {yolo_bbox[0]} {yolo_bbox[1]} {yolo_bbox[2]} {yolo_bbox[3]}\n"
                             )
 
-                            with open(
-                                label_file_path, "a", encoding="utf-8"
-                            ) as label_file:
-                                label_file.write(
-                                    f"{self.config.class_id} {yolo_bbox[0]} {yolo_bbox[1]} {yolo_bbox[2]} {yolo_bbox[3]}\n"
-                                )
-
-                    counter += 1
-                    print(f"Добавлен файл: {file_name}")
+                counter += 1
 
 
-if __name__ == "__main__":
+def run(**kwargs):
+    return run_config(Config(**kwargs))
+
+
+def run_config(config):
+    creator = YOLODatasetCreator(config)
+    creator.create_dataset()
+    return creator
+
+
+def main():
     config = Config()
     parser = argparse.ArgumentParser(description="Утилита для создания YOLO датасета")
 
@@ -125,7 +145,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--valid",
         type=int,
-        default=config.valid_percent,
+        default=config.valid,
         help="Процент данных для валидации",
     )
     parser.add_argument(
@@ -137,5 +157,9 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     config.__dict__.update(vars(args))
-    creator = YOLODatasetCreator(config)
-    creator.create_dataset()
+
+    run_config(config)
+
+
+if __name__ == "__main__":
+    main()
